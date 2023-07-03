5776ff392c660bcda10ab06adb147c7132036bd6
EagleVector <kumarsubhambili3@gmail.com>
2023-07-03 11:52:58 +0530

    Adding custom clickCount + visitHistory analytics to the service

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { connectToMongoDB } = require('./connect');
 const urlRoute = require("./routes/url");
+const URL = require('./models/url')
 
 const app = express();
 const PORT = 8001;
@@ -9,5 +10,22 @@ connectToMongoDB("mongodb://127.0.0.1:27017/url_short")
 .then(() => console.log("MongoDB Connected")
 );
 
-app.use("/url", urlRoute)
+app.use(express.json());
+
+app.use("/url", urlRoute);
+
+app.get('/:shortId', async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate({
+    shortId
+  }, { $push: {
+    visitHistory: {
+      timestamp: Date.now()
+    },
+  },
+}
+);
+res.redirect(entry.redirectURL);
+})
+
 app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`));
