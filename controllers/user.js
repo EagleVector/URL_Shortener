4c96190e7f0a40adf10749c472288e667d016afe
EagleVector <kumarsubhambili3@gmail.com>
2023-07-04 19:52:03 +0530

    LogIn page

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,6 @@
+const { v4: uuidv4 } = require('uuid');
 const User = require('../models/user');
+const { setUser } = require('../service/auth')
 
 async function handleUserSignUp(req, res) {
   const { name, email, password } = req.body;
@@ -10,6 +12,25 @@ async function handleUserSignUp(req, res) {
   return res.redirect("/");
 };
 
+async function handleUserLogin(req, res) {
+  const { email, password } = req.body;
+  const user = await User.findOne({
+    email,
+    password
+  });
+  if (!user) {
+    return res.render('login', {
+      error: "Invalid Username or Password",
+    });
+  }
+
+  const sessionId = uuidv4();
+  setUser(sessionId, user);
+  res.cookie('uid', sessionId);
+  return res.redirect("/");
+};
+
 module.exports = {
-  handleUserSignUp
+  handleUserSignUp,
+  handleUserLogin
 };
