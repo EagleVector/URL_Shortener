fda2864f1ebc6e1b7cd32fd94ff51341f1e29868
EagleVector <kumarsubhambili3@gmail.com>
2023-07-04 19:55:22 +0530

    Adding Cookies and user auth

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const path = require('path');
+const cookieParser = require("cookie-parser");
 
 const { connectToMongoDB } = require('./connect');
-
+const { restrictToLoggedinUserOnly } = require('./middlewares/auth') 
 const URL = require('./models/url');
 
 const urlRoute = require("./routes/url");
@@ -21,8 +22,9 @@ app.set("views", path.resolve("./views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.use("/url", urlRoute);
+app.use("/url", restrictToLoggedinUserOnly, urlRoute);
 app.use("/user", userRoute);
 app.use("/", staticRoute);
 
